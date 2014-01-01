658478989c46b92d3c9a324e4ab727ca703ef855
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-01-01 18:24:07 +0530

    smdk4412-common: Touchwake Control
    Change-Id: I3a9c0b8a112df8baea3b5510f133d1e9bd4a82ac

--- DeviceSettings/src/org/omnirom/device/ScreenFragmentActivity.java
+++ DeviceSettings/src/org/omnirom/device/ScreenFragmentActivity.java
@@ -23,16 +23,20 @@ import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.util.Log;
 
 import org.omnirom.device.R;
 
-public class ScreenFragmentActivity extends PreferenceFragment {
+public class ScreenFragmentActivity extends PreferenceFragment implements
+        OnPreferenceChangeListener {
 
     private static final String PREF_ENABLED = "1";
     private static final String TAG = "DeviceSettings_Screen";
@@ -46,9 +50,16 @@ public class ScreenFragmentActivity extends PreferenceFragment {
 
     private static boolean sSPenSupported;
     private static boolean sTouchkeySupport;
-
     private static final String FILE_TOUCHKEY_BRIGHTNESS = "/sys/class/sec/sec_touchkey/brightness";
     private static final String FILE_TOUCHKEY_DISABLE = "/sys/class/sec/sec_touchkey/force_disable";
+
+    private SwitchPreference mTouchwakeEnable;
+    private SeekBarPreference mTouchwakeTimeout;
+    private static final String TOUCHWAKE_CATEGORY = "category_power_menu";
+    private static final String KEY_TOUCHWAKE_ENABLE = "touchwake_enable";
+    private static final String KEY_TOUCHWAKE_TIMEOUT = "touchwake_timeout";
+    private static final String FILE_TOUCHWAKE_ENABLE = "/sys/devices/virtual/misc/touchwake/enabled";
+    private static final String FILE_TOUCHWAKE_TIMEOUT = "/sys/devices/virtual/misc/touchwake/delay";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -89,6 +100,26 @@ public class ScreenFragmentActivity extends PreferenceFragment {
             mTouchKeyTimeout.setEnabled(false);
         }
 
+        /* Touchwake */
+        mTouchwakeEnable = (SwitchPreference) findPreference(KEY_TOUCHWAKE_ENABLE);
+        mTouchwakeTimeout = (SeekBarPreference) findPreference(KEY_TOUCHWAKE_TIMEOUT);
+
+        if (!isSupported(FILE_TOUCHWAKE_ENABLE)) {
+            mTouchwakeEnable.setEnabled(false);
+            mTouchwakeEnable.setSummary(R.string.kernel_does_not_support);
+
+            mTouchwakeTimeout.setEnabled(false);
+            mTouchwakeTimeout.setSummary(R.string.kernel_does_not_support);
+        } else {
+            boolean b = Boolean.valueOf(Utils.readOneLine(FILE_TOUCHWAKE_ENABLE));
+            mTouchwakeEnable.setChecked(b);
+            mTouchwakeEnable.setOnPreferenceChangeListener(this);
+
+            int i = Integer.parseInt(Utils.readOneLine(FILE_TOUCHWAKE_TIMEOUT));
+            mTouchwakeTimeout.setValue(i / 1000);
+            mTouchwakeTimeout.setOnPreferenceChangeListener(this);
+        }
+
         /* S-Pen */
         String spenFilePath = res.getString(R.string.spen_sysfs_file);
         sSPenSupported = SPenPowerSavingMode.isSupported(spenFilePath);
@@ -119,6 +150,26 @@ public class ScreenFragmentActivity extends PreferenceFragment {
         return true;
     }
 
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mTouchwakeEnable) {
+            boolean b = (Boolean) newValue;
+            mTouchwakeEnable.setChecked(b);
+
+            Utils.writeValue(FILE_TOUCHWAKE_ENABLE, b);
+
+            return true;
+        } else if (preference == mTouchwakeTimeout) {
+            int i = (Integer) newValue;
+            mTouchwakeTimeout.setValue(i);
+
+            String s = Integer.toString (i * 1000);
+            Utils.writeValue(FILE_TOUCHWAKE_TIMEOUT, s);
+
+            return true;
+        }
+        return false;
+    }
+
     public static boolean isSupported(String FILE) {
         return Utils.fileExists(FILE);
     }
@@ -129,5 +180,13 @@ public class ScreenFragmentActivity extends PreferenceFragment {
 
         Utils.writeValue(FILE_TOUCHKEY_DISABLE, light ? "0" : "1");
         Utils.writeValue(FILE_TOUCHKEY_BRIGHTNESS, light ? "1" : "2");
+
+        if (isSupported(FILE_TOUCHWAKE_ENABLE)) {
+            boolean b = sharedPrefs.getBoolean(KEY_TOUCHWAKE_ENABLE, false);
+            Utils.writeValue(FILE_TOUCHWAKE_ENABLE, b);
+
+            int i = sharedPrefs.getInt(KEY_TOUCHWAKE_TIMEOUT, 10) * 1000;
+            Utils.writeValue(FILE_TOUCHWAKE_TIMEOUT, Integer.toString(i));
+        }
     }
 }
