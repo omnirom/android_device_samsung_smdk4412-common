001c377c7d865c35ec7eaf792fb29164098b40a4
JustArchi <JustArchi@JustArchi.net>
2014-11-13 01:50:33 +0100

    EXPERIMENTAL: Let's try to fix Wi-Fi
    Conflicts:
    	common.mk
    
    Change-Id: I560d90b73a7e6a314eb608dcf59456d5889921ce

--- common.mk
+++ common.mk
@@ -67,17 +67,22 @@ PRODUCT_PACKAGES := \
     audio.r_submix.default \
     audio.usb.default \
     com.android.future.usb.accessory \
+    dhcpcd.conf \
     gralloc.exynos4 \
+    hostapd \
+    hostapd_default.conf \
     hwcomposer.exynos4 \
     libfimg \
     libnetcmdiface \
     libsecion \
     libsync \
+    libwpa_client \
     lights.exynos4 \
     macloader \
     tinymix \
     SamsungServiceMode \
-    DeviceSettings
+    DeviceSettings \
+    wpa_supplicant
 
 ifneq ($(TARGET_NO_CAM_FLASH) ,true)
 PRODUCT_PACKAGES += \
