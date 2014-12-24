e6816d8310f47e1e765b0c2d6e0a87010a056b61
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-12-24 13:01:48 +0530

    smdk4412-common: switch to opensource healthd-charger
    Conflicts:
    	common.mk
    	proprietary-files.txt
    	rootdir/init.smdk4x12.rc
    
    Change-Id: I32aeb89f0c4acb0ba50f1f03a87d5e55386a05ca

--- common.mk
+++ common.mk
@@ -132,6 +132,18 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     librs_jni
 
+# Wifi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
