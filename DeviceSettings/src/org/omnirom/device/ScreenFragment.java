1b2f8230f7a5612a25f7eba6c48d2a982ac5fc56
Christian Balster <christian.balster@gmail.com>
2015-06-14 18:22:09 +0200

    smdk4412-common: DeviceSettings: rework ui + materialize
    - replace deprecated actionbar tabs with PagerTabStrip
    - follow android guidelines regarding switch/checkbox preferences
      and the usage of the summary line
    - reword some options to clarify their use
    - basic translation cleanup
    
    Change-Id: I0001621b24b8b60af4435f64101318869c16da56

--- DeviceSettings/src/org/omnirom/device/ScreenFragment.java
+++ DeviceSettings/src/org/omnirom/device/ScreenFragment.java
@@ -43,9 +43,9 @@ public class ScreenFragment extends PreferenceFragment {
             findPreference(mDNIeMode.KEY_MDNIE_MODE).setEnabled(false);
         }
 
-        // if (!mDNIeNegative.isSupported(context)) {
-        //     findPreference(mDNIeNegative.KEY_MDNIE_NEGATIVE).setEnabled(false);
-        // }
+        if (!mDNIeNegative.isSupported(context)) {
+            findPreference(mDNIeNegative.KEY_MDNIE_NEGATIVE).setEnabled(false);
+        }
 
         /* LED */
         if (!LedFade.isSupported(context)) {
@@ -82,7 +82,7 @@ public class ScreenFragment extends PreferenceFragment {
         LedFade.restore(context);
         mDNIeScenario.restore(context);
         mDNIeMode.restore(context);
-        // mDNIeNegative.restore(context);
+        mDNIeNegative.restore(context);
         SPenPowerSavingMode.restore(context);
         Touchkey.restore(context);
         TouchkeyTimeout.restore(context);
