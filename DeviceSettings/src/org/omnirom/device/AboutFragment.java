747ce12157457d0e3fe0a624cb58d2f358ca218f
Andreas Blaesius <skate4life@gmx.de>
2015-07-19 16:51:11 +0200

    DeviceSettings: Add "About Tab"
    Thanks to Max Weninger for helping me getting this work
    
    Change-Id: Icb31303d650eb9531d4d12dfd4089c68c053c261

---
+++ DeviceSettings/src/org/omnirom/device/AboutFragment.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.omnirom.device;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.*;
+
+import org.omnirom.device.R;
+
+public class AboutFragment extends Fragment {
+
+    private static final String PREF_ENABLED = "1";
+    private static final String TAG = "GalaxyTab2Settings_About";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup root,
+            Bundle savedInstanceState) {
+        super.onCreateView(inflater, root, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.about_activity, root, false);
+        return view;
+    }
+}
